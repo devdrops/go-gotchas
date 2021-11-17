0c9330cc168b01d20af569645b5e2d1e06d69ee8
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2021-11-17 18:48:09 -0300

    Gotcha 1:
    Organizando comentários e explicações.

--- gotcha-assignment-entry-nil-map/main.go
+++ gotcha-assignment-entry-nil-map/main.go
@@ -2,39 +2,39 @@ package main
 
 import "fmt"
 
-/*
-A declaração abaixo resulta em panic:
-
-```
-panic: assignment to entry in nil map
-```
-
-Por quê?
-Temos a variável criada na declaração `var`, temos a atribuição de valor. O que
-falta aqui é o uso de **make**.
-
-Todo map precisa ser inicializado, seja com a função make ou um map literal
-antes de inserir elementos. Todo map tem nil por padrão até ser inicializado, e
-enquanto for assim, elementos não podem ser inseridos nele.
-
-Referência de leitura:
-
-- https://yourbasic.org/golang/maps-explained/
-*/
-
 func main() {
-	// Código original:
-	//var m map[string]float64
-	//m["pi"] = 3.1416
+	/*
+	A declaração abaixo resulta em panic:
+	```
+	panic: assignment to entry in nil map
+	```
+	var m map[string]float64
+	m["pi"] = 3.1416
 
-	// Código com uso de make:
+	Por quê?
+	Tem a variável criada na declaração `var`, tem a atribuição de valor.
+	O que falta aqui é a inicialização do map.
+
+	Todo map precisa ser inicializado, seja com a função make ou um map
+	literal antes de inserir elementos. Todo map tem nil por padrão até ser
+	inicializado, e enquanto for assim, elementos não podem ser inseridos
+	nele.
+	*/
+
+	// map inicializado com uso de make:
 	m := make(map[string]float64)
 	m["pi"] = 3.1416
 
-	// Código com map literal:
+	// map inicializado de forma literal:
 	var om = map[string]float64{
 		"pi": 3.1416,
 		"aaa": 1.2345,
 	}
 	fmt.Println(om)
+
+	/*
+	Referência de leitura:
+
+	- https://yourbasic.org/golang/maps-explained/
+	*/
 }
